d5c4feb8585389d66614c7b5ef7515d4717dbbd1
Beny <Beny@Beny.local>
2011-08-02 19:30:40 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laintrusa::Application.config.secret_token = '9eb842f3ff8c2166bca380a001385bda9a1105bb5861fbae9958b0fa0a83cbe4fe362e7ee3f3d5aee55a89d7d179986f421939f9a5fb1e96add305ed4342c606'
